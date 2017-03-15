a2d09e5ca786e1b21076c49cdd19bc627dba55a4
xufeng <1054993687>
2017-03-15 20:07:28 +0800

    添加测试类

---
+++ api/src/main/java/com/xf/college/api/Test.java
@@ -0,0 +1,10 @@
+package com.xf.college.api;
+
+/**
+ * Created by xufeng on 2017/3/7.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ service/src/main/java/com/xf/college/api/service/Test.java
@@ -0,0 +1,12 @@
+package com.xf.college.api.service;
+
+/**
+ * Created by xufeng on 2017/3/7.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.printf("");
+    }
+
+
+}
