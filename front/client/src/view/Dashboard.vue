6623d93264bb22aa3c9036e3becb5329d586ac3d
xufeng <xf44397@ly.com>
2017-03-17 17:14:23 +0800

    add vue cli

---
+++ front/client/src/view/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <content-module name="dashboard">
+    Dashboard
+    <el-button type="info" @click.native="sendMessage">Send hello</el-button>
+  </content-module>
+</template>
+<script>
+export default {
+  methods: {
+    sendMessage () {
+
+    }
+  }
+
+}
+</script>
---
+++ front/client/src/view/ThingList.vue
@@ -0,0 +1,33 @@
+<template>
+  <content-module name="things">
+
+  </content-module>
+</template>
+<script>
+export default {
+  data () {
+
+  },
+  methods: {
+
+  },
+  created () {
+    //this.fetch()
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import "../assets/css/variable"
+.box-card
+  display inline-block
+  width 20rem
+  height 15rem
+  margin .5rem
+  .icon
+    float right
+    margin-left .5rem
+    color $color-silver
+    cursor pointer
+    &:hover
+      color $color-primary
+</style>
