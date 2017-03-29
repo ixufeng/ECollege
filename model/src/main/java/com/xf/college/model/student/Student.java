c232ecb5cc6457f6b5c85f6c7a4175426d111823
xufeng <xf44397@ly.com>
2017-03-29 16:07:39 +0800

    add

---
+++ model/src/main/java/com/xf/college/model/student/Student.java
@@ -0,0 +1,31 @@
+package com.xf.college.model.student;
+
+/**
+ * Created by xf44397 on 2017/3/21.
+ */
+public class Student {
+
+    private String name;
+    private String id;
+
+    public Student(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ model/src/main/java/com/xf/college/model/teacher/Teacher.java
@@ -0,0 +1,10 @@
+package com.xf.college.model.teacher;
+
+/**
+ * Created by xf44397 on 2017/3/21.
+ */
+public class Teacher {
+    private String name;
+    private String id;
+
+}
