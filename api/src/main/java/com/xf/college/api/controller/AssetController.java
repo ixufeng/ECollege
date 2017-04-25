106b520461e81c193e4cdbc30419ee38b0de9d5a
xufeng <1054993687>
2017-04-25 23:30:32 +0800

    添加查询资产接口

---
+++ api/src/main/java/com/xf/college/api/controller/AssetController.java
@@ -0,0 +1,40 @@
+package com.xf.college.api.controller;
+
+import com.xf.college.model.apiwrapper.APIResult;
+import com.xf.college.service.assets.AssetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by xufeng on 2017/4/25.
+ */
+@RequestMapping("/asset")
+@RestController
+public class AssetController extends BaseController{
+
+    @Autowired
+    private AssetService assetService;
+    /**
+     * 获取所有资产接口 对管理员可见
+     * @return
+     */
+    @RequestMapping("/all")
+    public APIResult getAllAssets() {
+        int auth = getAuth();
+       /* if (auth != Auth.ADMIN) {
+            return asUnAuthError("仅管理员可见！");
+        }*/
+        return asSuccess(assetService.selectAll());
+    }
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public APIResult getUserAsset(
+            @RequestParam("userId") String userId
+    ) {
+        int auth = getAuth();
+
+        return asSuccess(assetService.selectByUserId(userId));
+    }
+}
---
+++ model/src/main/java/com/xf/college/model/laboratory/Asset.java
@@ -0,0 +1,209 @@
+package com.xf.college.model.laboratory;
+
+import java.util.Date;
+
+/**
+ * Created by xufeng on 2017/3/25.
+ */
+public class Asset {
+    private String id;  //编号
+    private String name;
+    private String typeId; //分类号
+    private Date beginTime;
+    private int valid;    //现状
+    private int usedCount;  //使用次数
+    private String des;  //描述
+    private String remark;  //备注
+    private String imgs;  //
+
+    //新增
+    private String model; //型号
+    private String specification; //规格
+    private double price;  //单价
+    private String nationality; //国别
+    private String factoryName;
+    private Date buyTime;
+    private String userId;  //领用人编号
+    private String userName;//for vo
+    private String handleUserId;//经手人
+    private String handleUserName;// for vo
+    private Date incoming;//入库时间
+    private Date lastUseTime; //最后一次使用时间
+
+
+
+    public String getImgs() {
+        return imgs;
+    }
+    public String[] getImgs1() {
+        return imgs==null?new String[0]:imgs.split(",");
+    }
+    public void addImg(String img) {
+        if (img==null) return;
+        if (this.imgs==null) this.imgs = img;
+        else this.imgs += "," + img;
+    }
+    public void setImgs(String imgs) {
+        this.imgs = imgs;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(String typeId) {
+        this.typeId = typeId;
+    }
+
+    public Date getBeginTime() {
+        return beginTime;
+    }
+
+    public void setBeginTime(Date beginTime) {
+        this.beginTime = beginTime;
+    }
+
+    public int getValid() {
+        return valid;
+    }
+
+    public void setValid(int valid) {
+        this.valid = valid;
+    }
+
+    public int getUsedCount () {
+        return usedCount;
+    }
+
+    public void setUsedCount(int usedCount) {
+        this.usedCount = usedCount;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(String specification) {
+        this.specification = specification;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getFactoryName() {
+        return factoryName;
+    }
+
+    public void setFactoryName(String factoryName) {
+        this.factoryName = factoryName;
+    }
+
+    public Date getBuyTime() {
+        return buyTime;
+    }
+
+    public void setBuyTime(Date buyTime) {
+        this.buyTime = buyTime;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getHandleUserId() {
+        return handleUserId;
+    }
+
+    public void setHandleUserId(String handleUserId) {
+        this.handleUserId = handleUserId;
+    }
+
+    public String getHandleUserName() {
+        return handleUserName;
+    }
+
+    public void setHandleUserName(String handleUserName) {
+        this.handleUserName = handleUserName;
+    }
+
+    public Date getIncoming() {
+        return incoming;
+    }
+
+    public void setIncoming(Date incoming) {
+        this.incoming = incoming;
+    }
+
+    public Date getLastUseTime() {
+        return lastUseTime;
+    }
+
+    public void setLastUseTime(Date lastUseTime) {
+        this.lastUseTime = lastUseTime;
+    }
+}
