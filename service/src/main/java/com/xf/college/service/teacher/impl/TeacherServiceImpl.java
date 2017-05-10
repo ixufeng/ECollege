9a190cdd1786b9fc35c927f75d8e364d01ea9c6d
xufeng <1054993687>
2017-05-10 23:04:44 +0800

    bug fixed

--- service/src/main/java/com/xf/college/service/teacher/impl/TeacherServiceImpl.java
+++ service/src/main/java/com/xf/college/service/teacher/impl/TeacherServiceImpl.java
@@ -1,6 +1,7 @@
 package com.xf.college.service.teacher.impl;
 
 import com.xf.college.common.CommonResult;
+import com.xf.college.common.utils.CommonUtils;
 import com.xf.college.dao.teacher.TeacherDao;
 import com.xf.college.model.teacher.Teacher;
 import com.xf.college.service.RedisManager;
@@ -34,10 +35,18 @@ public class TeacherServiceImpl implements TeacherService {
             if (oldTeacher == null) {
                 return CommonResult.QUERY_ERROR;
             }
-            if (teacher.getMail() != null) {
+            if (teacher.getMail() != null && !"".equals(teacher.getMail())) {
+                boolean b = CommonUtils.checkMail(teacher.getMail());
+                if (!b) {
+                    return "邮箱格式不合法";
+                }
                 oldTeacher.setMail(teacher.getMail());
             }
-            if (teacher.getPhone() != null) {
+            if (teacher.getPhone() != null && !"".equals(teacher.getPhone())) {
+                boolean b = CommonUtils.checkPhone(teacher.getPhone());
+                if (!b) {
+                    return "手机号码不合法";
+                }
                 oldTeacher.setPhone(teacher.getPhone());
             }
             int result = teacherDao.update(oldTeacher);
